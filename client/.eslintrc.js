c00b436bb6b37a3aead3f8ad1e3246d05c35a7d4
Jordan Sim-Smith <jordansimsmith@gmail.com>
2021-05-08 11:40:05 +1200

    Configure eslint and prettier

---
+++ client/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+  globals: {
+    React: 'writable',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
