bea19db4038df9e5a347e69ccf221a7166bbc9a2
Jordan Sim-Smith <jordansimsmith@gmail.com>
2021-05-14 10:14:10 +1200

    Add migration for adding subscriptions

---
+++ server/src/FootballSubscriber.Infrastructure/Data/Migrations/20210513220916_AddSubscription.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FootballSubscriber.Infrastructure.Data.Migrations
+{
+    public partial class AddSubscription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscription",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TeamId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscription", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subscription_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscription_TeamId_UserId",
+                table: "Subscription",
+                columns: new[] { "TeamId", "UserId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscription");
+        }
+    }
+}
