ad640af38d7b4a45a8b3b955092563b1d3077592
Jordan Sim-Smith <jordansimsmith@gmail.com>
2022-05-02 13:04:33 +1200

    Set minimum TTL value for images

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   images: {
-    domains: ['sportsgroundproduction.blob.core.windows.net']
+    domains: ['sportsgroundproduction.blob.core.windows.net'],
+    minimumCacheTTL: 60*60*24 // one day
   }
 }
