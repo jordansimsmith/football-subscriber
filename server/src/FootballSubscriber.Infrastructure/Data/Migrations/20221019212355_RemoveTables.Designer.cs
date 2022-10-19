53caf49976330b6876ed3fc3f00c74372bfd43bb
Jordan Sim-Smith <jordansimsmith@gmail.com>
2022-10-20 10:24:57 +1300

    Add migration to drop all tables

--- server/src/FootballSubscriber.Infrastructure/Data/Migrations/20221019212355_RemoveTables.Designer.cs
+++ server/src/FootballSubscriber.Infrastructure/Data/Migrations/20221019212355_RemoveTables.Designer.cs
@@ -2,6 +2,7 @@
 using FootballSubscriber.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FootballSubscriber.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(FootballSubscriberContext))]
-    partial class FootballSubscriberContextModelSnapshot : ModelSnapshot
+    [Migration("20221019212355_RemoveTables")]
+    partial class RemoveTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
